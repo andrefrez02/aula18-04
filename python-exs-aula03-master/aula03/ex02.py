41fe64c76d9d2970c7acd51ca1939f9dd7d0a2d7
andrefrez02 <andreluizfrezarinidiasdasilva@gmail.com>
2022-04-19 09:08:03 -0300

    commit

--- python-exs-aula03-master/aula03/ex02.py
+++
@@ -1,5 +0,0 @@
-horas = int(input('Entrada:\nDigite um valor em horas: '))
-
-minutos = horas * 60
-
-print(f'\nSaída\n{horas} horas é igual a: {minutos} minutos.')
--- python-exs-aula03-master/aula04/ex01.py
+++
@@ -1,7 +0,0 @@
-num = int(input('Entrada:\nDigite um número inteiro: '))
-
-print('\nSaída:')
-if num > 9 or num < 0:
-    print('Valor incorreto!')
-else:
-    print('Valor correto!')
